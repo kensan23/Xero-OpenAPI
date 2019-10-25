19e41eb276e253783a6211099012efa7f62067fe
kensan23 <9150185+kensan23@users.noreply.github.com>
2019-10-25 12:04:04 +1100

    Update dotnet-oauth2-accounting.sh

--- dotnet-oauth2-accounting.sh
+++ dotnet-oauth2-accounting.sh
@@ -24,6 +24,6 @@ openapi-generator $ags
 rm -rf generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/project.json
 sed -e '/<TargetFrameworkIdentifier>.NETStandard<\/TargetFrameworkIdentifier>/d' -i generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/Xero.NetStandard.OAuth2.csproj
 sed -e 's/<TargetFrameworkVersion>v2.0<\/TargetFrameworkVersion>/<TargetFramework>netstandard2.0<\/TargetFramework>/g' -i generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/Xero.NetStandard.OAuth2.csproj
-sed -e 's/string? /string /g' -i generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/model/
-sed -e 's/DateTime?? /DateTime? /g' -i generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/model/
-sed -e 's/List<string>? /List<string> /g' -i generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/model/
+find generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/Model/*.cs -type f | xargs sed -e 's/string? /string /g' -i
+find generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/Model/*.cs -type f | xargs sed -e 's/DateTime?? /DateTime?' /g -i
+find generator/output/csharp-netcore/accounting/src/Xero.NetStandard.OAuth2/Model/*.cs -type f | xargs sed -e 's/List<string>? /List<string> /g' -i
