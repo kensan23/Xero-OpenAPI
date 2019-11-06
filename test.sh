dc801c063e9652e397e48856af6b490a6bb0a58f
kensan23 <9150185+kensan23@users.noreply.github.com>
2019-11-07 09:13:09 +1100

    Folder changes

--- test.sh
+++
@@ -1,4 +0,0 @@
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -t /generator/modules/csharp-netcore -g csharp-netcore -i https://raw.githubusercontent.com/XeroAPI/Xero-OpenAPI/oauth2/accounting-yaml/Xero_accounting_2.0.0_swagger.yaml -o ./generator/output/csharp-netcore/accounting \
- #-c ./dotnet-oauth2-accounting.json \
- -D debugModels=false \
--D hideGenerationTimestamp=true
