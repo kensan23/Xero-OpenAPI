fdc57da7ee43e7699f70f95145a6e5ae46f91a27
kensan23 <9150185+kensan23@users.noreply.github.com>
2019-12-05 14:51:45 +1100

    Update dotnet-oauth2-accounting.sh

--- generator/scripts/dotnet-oauth2-accounting.sh
+++ generator/scripts/dotnet-oauth2-accounting.sh
@@ -12,7 +12,7 @@ ags="generate
 -i https://raw.githubusercontent.com/XeroAPI/Xero-OpenAPI/oauth2/accounting-yaml/Xero_accounting_2.0.0_swagger.yaml 
 -g csharp-netcore
 -o ./generator/output/csharp-netcore/accounting
--c dotnet-oauth2-accounting.json
+-c ./generator/scripts/dotnet-oauth2-accounting.json
 -p debugModels=false
 -p hideGenerationTimestamp=true
 $@"
